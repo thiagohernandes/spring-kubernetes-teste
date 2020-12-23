4927423472d9ab8c376ec5418660795c562c496b
Thiago Hernandes de Souza <thiago.hernandes@dbccompany.com.br>
2020-12-23 11:17:55 -0300

    Configuração inicial

---
+++ src/main/java/com/hernandes/SpringKubernetesTesteApplication.java
@@ -0,0 +1,13 @@
+package com.hernandes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKubernetesTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKubernetesTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hernandes/SpringKubernetesTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hernandes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKubernetesTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
